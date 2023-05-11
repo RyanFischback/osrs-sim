2dc9bc8a50bbeb048f01c109b012a9c840b7f5e4
rfischback <ryan-fischback@hotmail.com>
2023-05-11 15:20:56 -0300

    scripts fixed

--- backend/db/scripts/Create.sql
+++ backend/db/scripts/Create.sql
@@ -1,26 +1,26 @@
 -- Drop the database if it exists
-DROP DATABASE IF EXISTS mydatabase;
+DROP SCHEMA IF EXISTS "osrs-sim" CASCADE;
 
 -- Create the database
-CREATE DATABASE mydatabase;
+CREATE SCHEMA IF NOT EXISTS "osrs-sim";
 
 -- Create the Player table
-CREATE TABLE Player (
+CREATE TABLE "osrs-sim".Player (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL
 );
 
 -- Create the Items table
-CREATE TABLE Items (
+CREATE TABLE "osrs-sim".Items (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
     description VARCHAR(500) NOT NULL,
     imageURL VARCHAR(500) NOT NULL
 );
 
 -- Create the GrandExchangeItems (Auction House) table
-CREATE TABLE GrandExchangeItems (
+CREATE TABLE "osrs-sim".GrandExchangeItems (
     id SERIAL PRIMARY KEY,
     itemid INTEGER NOT NULL REFERENCES Items(id),
     price INTEGER NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE GrandExchangeItems (
 );
 
 -- Create the PlayerItems table
-CREATE TABLE PlayerItems (
+CREATE TABLE "osrs-sim".PlayerItems (
     id SERIAL PRIMARY KEY,
     playerid INTEGER NOT NULL REFERENCES Player(id),
     itemid INTEGER NOT NULL REFERENCES Items(id),
@@ -38,16 +38,16 @@ CREATE TABLE PlayerItems (
 );
 
 -- Create the Skills table
-CREATE TABLE Skills (
+CREATE TABLE "osrs-sim".Skills (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
     description VARCHAR(500) NOT NULL,
     imageURL VARCHAR(500) NOT NULL,
     maxlevel INTEGER NOT NULL
 );
 
 -- Create the PlayerSkills table
-CREATE TABLE PlayerSkills (
+CREATE TABLE "osrs-sim".PlayerSkills (
     id SERIAL PRIMARY KEY,
     playerid INTEGER NOT NULL REFERENCES Player(id),
     skillid INTEGER NOT NULL REFERENCES Skills(id),
--- backend/db/scripts/Data.sql
+++ backend/db/scripts/Data.sql
@@ -1,28 +1,28 @@
 -- Insert data into the Player table
-INSERT INTO Player (username, password) VALUES ('rfischback', 'password1');
+INSERT INTO "osrs-sim".Player (username, password) VALUES ('rfischback', 'password1');
 -- INSERT INTO Player (username, password) VALUES ('username2', 'password2');
 -- INSERT INTO Player (username, password) VALUES ('username3', 'password3');
 
 -- Insert data into the Items table
-INSERT INTO Items (name, description, imageURL) VALUES ('hammer', 'hammer', 'imageURL1');
-INSERT INTO Items (name, description, imageURL) VALUES ('pole', 'fishing pole', 'imageURL2');
-INSERT INTO Items (name, description, imageURL) VALUES ('pickaxe', 'description3', 'imageURL3');
+INSERT INTO "osrs-sim".Items (name, description, imageURL) VALUES ('hammer', 'hammer', 'imageURL1');
+INSERT INTO "osrs-sim".Items (name, description, imageURL) VALUES ('pole', 'fishing pole', 'imageURL2');
+INSERT INTO "osrs-sim".Items (name, description, imageURL) VALUES ('pickaxe', 'description3', 'imageURL3');
 
 -- Insert data into the PlayerItems table
-INSERT INTO PlayerItems (playerid, itemid, count) VALUES (1, 1, 1);
-INSERT INTO PlayerItems (playerid, itemid, count) VALUES (1, 2, 1);
-INSERT INTO PlayerItems (playerid, itemid, count) VALUES (1, 3, 1);
+INSERT INTO "osrs-sim".PlayerItems (playerid, itemid, count) VALUES (1, 1, 1);
+INSERT INTO "osrs-sim".PlayerItems (playerid, itemid, count) VALUES (1, 2, 1);
+INSERT INTO "osrs-sim".PlayerItems (playerid, itemid, count) VALUES (1, 3, 1);
 
 -- Insert data into the Skills table
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('smithing', 'smithing helps improve speed at which you do this', 'imageURL1', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('fishing', 'fishing helps improve speed at which you do this', 'imageURL2', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('mining', 'mining helps improve speed at which you do this', 'imageURL3', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('agility', 'agility helps improve speed at which you do this', 'imageURL3', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('herblore', 'herblore helps improve speed at which you do this', 'imageURL3', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('hunter', 'hunter helps improve speed at which you do this', 'imageURL3', 99);
-INSERT INTO Skills (name, description, imageURL, maxlevel) VALUES ('farming', 'farming helps improve speed at which you do this', 'imageURL3', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('smithing', 'smithing helps improve speed at which you do this', 'imageURL1', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('fishing', 'fishing helps improve speed at which you do this', 'imageURL2', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('mining', 'mining helps improve speed at which you do this', 'imageURL3', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('agility', 'agility helps improve speed at which you do this', 'imageURL3', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('herblore', 'herblore helps improve speed at which you do this', 'imageURL3', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('hunter', 'hunter helps improve speed at which you do this', 'imageURL3', 99);
+INSERT INTO "osrs-sim".Skills (name, description, imageURL, maxlevel) VALUES ('farming', 'farming helps improve speed at which you do this', 'imageURL3', 99);
 
 -- Insert data into the PlayerSkills table
-INSERT INTO PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 1, 3);
-INSERT INTO PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 2, 1);
-INSERT INTO PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 3, 2);
+INSERT INTO "osrs-sim".PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 1, 3);
+INSERT INTO "osrs-sim".PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 2, 1);
+INSERT INTO "osrs-sim".PlayerSkills (playerid, skillid, playercurrentlevel) VALUES (1, 3, 2);
